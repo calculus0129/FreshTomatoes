87ad62251b1581d539ed32be6574b763d9f044a5
calculus0129 <ryanbhang129@naver.com>
2024-04-19 16:05:54 +0900

    refactor: commentize 'user' related controller part.

--- milestone1/src/main/java/com/example/App/movieinfo/controller/Controller.java
+++ milestone1/src/main/java/com/example/App/movieinfo/controller/Controller.java
@@ -39,23 +39,23 @@ public class Controller {
     }
 
     // get movies with ratings higher or equal to given rating
-    @GetMapping("/ratings/{r}")
-    ResponseEntity<List<Movie>> MovieWithRating(@PathVariable Long r) {
-        if (r < 1 || r > 5) throw new MovieNotFoundException();
-
-        List<Long> movieIds = template.query(Rating.class).distinct("movieId").as(Long.class).all();
-        List<Long> resMovieIds = movieIds.stream()
-                .filter(a -> template.query(Rating.class).matching(query(where("movieId").is(a))).all()
-                        .stream()
-                        .mapToLong(Rating::getRating)
-                        .average().orElse(0) >= r)
-                .collect(Collectors.toList());
-        if (resMovieIds.isEmpty()) throw new MovieNotFoundException(r);
-        List<Movie> movies = template.query(Movie.class).matching(query(where("movieId").in(resMovieIds))).all();
-        if (movies.isEmpty()) throw new MovieNotFoundException(r);
-
-        return ResponseEntity.ok().body(movies);
-    }
+//    @GetMapping("/ratings/{r}")
+//    ResponseEntity<List<Movie>> MovieWithRating(@PathVariable Long r) {
+//        if (r < 1 || r > 5) throw new MovieNotFoundException();
+//
+//        List<Long> movieIds = template.query(Rating.class).distinct("movieId").as(Long.class).all();
+//        List<Long> resMovieIds = movieIds.stream()
+//                .filter(a -> template.query(Rating.class).matching(query(where("movieId").is(a))).all()
+//                        .stream()
+//                        .mapToLong(Rating::getRating)
+//                        .average().orElse(0) >= r)
+//                .collect(Collectors.toList());
+//        if (resMovieIds.isEmpty()) throw new MovieNotFoundException(r);
+//        List<Movie> movies = template.query(Movie.class).matching(query(where("movieId").in(resMovieIds))).all();
+//        if (movies.isEmpty()) throw new MovieNotFoundException(r);
+//
+//        return ResponseEntity.ok().body(movies);
+//    }
 
     @GetMapping("movies")
     public List<Movie> getAllMovies() {
@@ -69,48 +69,48 @@ public class Controller {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @PostMapping("/users")
-    ResponseEntity<?> MovieNewRating(@RequestBody Rating rating) {
-        Long r = rating.getRating();
-        Long id = rating.getUserId();
-        Long mid = rating.getMovieId();
-
-        if (r < 1 || r > 5) throw new MovieNotFoundException();
-
-        List<Movie> m = template.query(Movie.class).matching(query(where("movieId").is(mid))).all();
-        if (m.isEmpty()) throw new MovieNotFoundException(mid.toString());
-        List<Rating> ra = template.query(Rating.class).matching(query(where("movieId").is(mid).and("userId").is(id))).all();
-        if (!ra.isEmpty()) throw new RatingDoneException(m.get(0).getTitle());
-
-        Rating newRating = new Rating(id, mid, r);
-
-
-        EntityModel<Rating> entityModel = EntityModel.of(ratingRepository.save(newRating),
-                linkTo(methodOn(Controller.class).MovieNewRating(rating)).withSelfRel());
-
-        return ResponseEntity //
-                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()) //
-                .body(entityModel);
-    }
-
-    @PutMapping("/users/{x}")
-    ResponseEntity<?> replaceEmployee(@RequestBody Rating rating, @PathVariable Long x) {
-        Long r = rating.getRating();
-        Long id = x;
-        Long mid = rating.getMovieId();
-        if (r < 1 || r > 5) throw new MovieNotFoundException();
-
-        List<Movie> m = template.query(Movie.class).matching(query(where("movieId").is(mid))).all();
-        if (m.isEmpty()) throw new MovieNotFoundException(mid.toString());
-        List<Rating> ra = template.query(Rating.class).matching(query(where("movieId").is(mid).and("userId").is(id))).all();
-
-        Rating newRating = new Rating(id, mid, r);
-
-        EntityModel<Rating> entityModel = EntityModel.of(ratingRepository.save(newRating),
-                linkTo(methodOn(Controller.class).MovieNewRating(rating)).withSelfRel());
-
-        return ResponseEntity //
-                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()) //
-                .body(entityModel);
-    }
+//    @PostMapping("/users")
+//    ResponseEntity<?> MovieNewRating(@RequestBody Rating rating) {
+//        Long r = rating.getRating();
+//        Long id = rating.getUserId();
+//        Long mid = rating.getMovieId();
+//
+//        if (r < 1 || r > 5) throw new MovieNotFoundException();
+//
+//        List<Movie> m = template.query(Movie.class).matching(query(where("movieId").is(mid))).all();
+//        if (m.isEmpty()) throw new MovieNotFoundException(mid.toString());
+//        List<Rating> ra = template.query(Rating.class).matching(query(where("movieId").is(mid).and("userId").is(id))).all();
+//        if (!ra.isEmpty()) throw new RatingDoneException(m.get(0).getTitle());
+//
+//        Rating newRating = new Rating(id, mid, r);
+//
+//
+//        EntityModel<Rating> entityModel = EntityModel.of(ratingRepository.save(newRating),
+//                linkTo(methodOn(Controller.class).MovieNewRating(rating)).withSelfRel());
+//
+//        return ResponseEntity //
+//                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()) //
+//                .body(entityModel);
+//    }
+//
+//    @PutMapping("/users/{x}")
+//    ResponseEntity<?> replaceEmployee(@RequestBody Rating rating, @PathVariable Long x) {
+//        Long r = rating.getRating();
+//        Long id = x;
+//        Long mid = rating.getMovieId();
+//        if (r < 1 || r > 5) throw new MovieNotFoundException();
+//
+//        List<Movie> m = template.query(Movie.class).matching(query(where("movieId").is(mid))).all();
+//        if (m.isEmpty()) throw new MovieNotFoundException(mid.toString());
+//        List<Rating> ra = template.query(Rating.class).matching(query(where("movieId").is(mid).and("userId").is(id))).all();
+//
+//        Rating newRating = new Rating(id, mid, r);
+//
+//        EntityModel<Rating> entityModel = EntityModel.of(ratingRepository.save(newRating),
+//                linkTo(methodOn(Controller.class).MovieNewRating(rating)).withSelfRel());
+//
+//        return ResponseEntity //
+//                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()) //
+//                .body(entityModel);
+//    }
 }
