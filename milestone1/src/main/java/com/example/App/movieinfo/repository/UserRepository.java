8442b07556c4daf70ddc7f643f2e7fde917be633
calculus0129 <ryanbhang129@naver.com>
2024-04-23 20:56:14 +0900

    test: RatingController GET PUT

--- milestone1/src/main/java/com/example/App/movieinfo/repository/UserRepository.java
+++ milestone1/src/main/java/com/example/App/movieinfo/repository/UserRepository.java
@@ -6,11 +6,12 @@ import com.example.App.movieinfo.model.User;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
-    List<User> findByUserId(Long userId); // This method allows you to fetch a movie by its movieID.
+    Optional<User> findByUserId(Long userId); // This method allows you to fetch a movie by its movieID.
     List<User> findByGenderAndAgeAndOccupation(String gender, Long age, Long occupation);
     //void deleteByMovieId(Long movieId);
 }
