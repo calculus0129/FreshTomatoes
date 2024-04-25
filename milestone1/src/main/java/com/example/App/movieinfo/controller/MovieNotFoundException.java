9e9ad34304558ddb185d971d826c1baa5eb8c953
Chae eun <moonchaeeun03@gmail.com>
2024-04-25 21:00:45 +0900

    added viscontroller

--- milestone1/src/main/java/com/example/App/movieinfo/controller/MovieNotFoundException.java
+++ milestone1/src/main/java/com/example/App/movieinfo/controller/MovieNotFoundException.java
@@ -2,9 +2,6 @@ package com.example.App.movieinfo.controller;
 
 public class MovieNotFoundException extends RuntimeException {
 
-    MovieNotFoundException(String id) {
-        super("Could not find Movie with ID " + id);
-    }
-    MovieNotFoundException() {super("Rating scale is from 1 to 5");}
+    MovieNotFoundException() {super("Movie is not present");}
     MovieNotFoundException(Long r) {super("There's no movie with rating higher than or equal to " + r);}
 }
