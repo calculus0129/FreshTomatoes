2ffd596042df6982b8ea4c122748c9af9b8524fb
Chae eun <moonchaeeun03@gmail.com>
2024-04-25 22:52:36 +0900

    changed

--- milestone1/src/main/java/com/example/App/movieinfo/controller/MovieNotFoundException.java
+++ milestone1/src/main/java/com/example/App/movieinfo/controller/MovieNotFoundException.java
@@ -3,5 +3,4 @@ package com.example.App.movieinfo.controller;
 public class MovieNotFoundException extends RuntimeException {
 
     MovieNotFoundException() {super("Movie is not present");}
-    MovieNotFoundException(Long r) {super("There's no movie with rating higher than or equal to " + r);}
 }
